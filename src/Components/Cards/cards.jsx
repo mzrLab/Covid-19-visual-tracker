c93cb7fb9986caf55cc7fdfcf6396a3e01984718
Mz <rebel.king700@gmail.com>
2020-04-27 00:52:34 +0430

    prepare and make the basic components

---
+++ src/Components/Cards/cards.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Cards= ()=> {
+    return(
+        <h1>Hey ther!</h1>
+    )
+}
+
+export default Cards;
---
+++ src/Components/Charts/Chart.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Chart= ()=> {
+    return(
+        <h1>Hey ther!</h1>
+    )
+}
+
+export default Chart;
