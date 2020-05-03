8185bad2ecc010b81844d511280d77047b9bfb4b
Mz <rebel.king700@gmail.com>
2020-05-03 17:20:55 +0430

    customize data

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import Chart from './Components/Charts/Chart';
 import Country from './Components/Country/Country';
 import {dataFetch} from './api';
 
-function App() {
+class App extends React.Component{
+  async componentDidMount(){
+    const data = await dataFetch();
+    console.log(data);
+  }
+  render(){
   return (
     <div className={styles.container}>
       <Cards />
@@ -14,6 +19,7 @@ function App() {
       <Chart />
     </div>
   );
+}
 }
 
 export default App;
--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,14 @@ const url = "https://covid19.mathdro.id/api";
 
 export const dataFetch = async()=>{
     try{
-        const response = await axios.get(url);
-        return(response);
+        const {data:{confirmed , recovered , deaths , lastUpdate}} = await axios.get(url);
+        const modifiedData = {
+            confirmed: confirmed,
+            recovered: recovered,
+            deaths: deaths,
+            lastUpdate: lastUpdate,
+        }
+        return(modifiedData);
     }catch(error){
 
     }
