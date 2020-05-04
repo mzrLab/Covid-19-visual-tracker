5593d063dc9b542243800ba0584041e42a50167d
Mz <rebel.king700@gmail.com>
2020-05-04 16:35:16 +0430

    add the country select list and map data

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, {useState , useEffect} from "react";
+import {NativeSelect , FormControl} from '@material-ui/core';
+
+import styles from './Country.module.css';
+import {fetchCountriesData} from '../../api';
 
 const Country= ()=> {
+
+    const [fetchedCountries , setFetchCountries] = useState([]);
+    useEffect(()=>{
+        const fetchCountries = async ()=>{
+            setFetchCountries(await fetchCountriesData());
+        }
+        fetchCountries();
+    });
+
+    console.log(fetchedCountries);
+
     return(
-        <h1>Hey ther!</h1>
+        <FormControl className={styles.formControl}>
+            <NativeSelect>
+                {fetchedCountries.map((country , i) => <option key={i} value={country}>{country}</option>)}
+            </NativeSelect>
+        </FormControl>
     )
 }
 
